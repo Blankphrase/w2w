d8f76f36ca546f7ff44a74ab7f67f581c652ebeb
hakubaa <jakub.agatowski@gmail.com>
2016-10-20 00:13:02 +0200

    Add NewPlayingQuery model

--- tmdb/models.py
+++ tmdb/models.py
@@ -7,21 +7,9 @@ from tmdb.util import tmdb_request
 MIN_UPDATE_LEVEL = 0
 POPULAR_UPDATE_LEVEL = 1
 SEARCH_UPDATE_LEVEL = 1
+NOWPLAYING_UPDATE_LEVEL = 1
 MOVIE_UPDATE_LEVEL = 2
 MAX_UPDATE_LEVEL = 2
-
-
-# data = self._download_movie_data(id)
-# data["update_level"] = MAX_UPDATE_LEVEL
-# fields_to_update = [
-#     field.name for field in Movie._meta.get_fields()\
-#         if field.name != "id"
-# ]
-# Movie.objects.filter(id = id).update(**{
-#     key: value for key, value in data.items()\
-#         if key in fields_to_update
-#     })  
-
 
 
 class Movie(models.Model):
@@ -35,11 +23,19 @@ class Movie(models.Model):
         return self.title
 
     @staticmethod
-    def save_movie_in_db(id):
-        data = tmdb_request(method = "GET", path = "movie/%s" % (id))
-        if data.get("status_code", None) == 34:
-            raise Movie.DoesNotExist()
-        data["update_level"] = MOVIE_UPDATE_LEVEL      
+    def save_movie_in_db(id, data_for_update = None):
+        if data_for_update:
+            fields_to_update = [
+                field.name for field in Movie._meta.get_fields()
+            ]
+            data = { key: value for key, value in data_for_update.items()
+                        if key in fields_to_update }
+        else:
+            data = tmdb_request(method = "GET", path = "movie/%s" % (id))
+            if data.get("status_code", None) == 34:
+                raise Movie.DoesNotExist()
+            data["update_level"] = MOVIE_UPDATE_LEVEL 
+
         movie, created = Movie.objects.update_or_create(
             id = id, defaults = data
         )    
@@ -91,11 +87,46 @@ class NowPlayingQuery(models.Model):
         return "page: {0} ({1})".format(self.page, self.timestamp)
 
     @staticmethod
-    def get(page):
+    def get(page, force_update = False):
         try:
+            if force_update:
+                raise NowPlayingQuery.DoesNotExist()
+
             npq = NowPlayingQuery.objects.get(page = page)
-        except:
-            data = tmdb_request(method = "GET", path = "movie/nowplaying", 
-                params = {"page": 1})
-            npq = NowPlayingQuery.objects.create(page = page)
+            # Update NowPlayginQuery when at least one movie is below
+            # required update level (it should not happen in practice)
+            if any(map(
+                lambda update_level: update_level < NOWPLAYING_UPDATE_LEVEL,
+                npq.movies.values_list("update_level", flat=True)
+            )):
+                raise NowPlayingQuery.DoesNotExist()
+        except NowPlayingQuery.DoesNotExist:
+            data = tmdb_request(method = "GET", path = "movie/now_playing", 
+                params = {"page": page})
+            
+            # Filter already updated movies
+            updated_movies = Movie.objects.filter(
+                id__in=[ int(movie["id"]) for movie in data["results"] ]
+            ).filter(update_level__gte=NOWPLAYING_UPDATE_LEVEL)
+            updated_ids = [ movie.id for movie in updated_movies ]
+
+            npq, created = NowPlayingQuery.objects.update_or_create(
+                page = page,
+                defaults = {
+                    "total_pages": data["total_pages"],
+                    "total_results": data["total_results"]
+                }
+            )
+            if not created:
+                npq.movies.clear()
+            npq.movies.add(*updated_movies)
+                        
+            for movie in data["results"]:
+                if movie["id"] not in updated_ids:
+                    movie["update_level"] = NOWPLAYING_UPDATE_LEVEL
+                    movie_db = Movie.save_movie_in_db(
+                        id = movie["id"],
+                        data_for_update = movie
+                    )
+                    npq.movies.add(movie_db)
         return npq
--- tmdb/tests/test_models.py
+++ tmdb/tests/test_models.py
@@ -11,23 +11,6 @@ from unittest.mock import patch
 import datetime
 
 
-class TMDBModelsTest(TestCase):
-
-    def test_create_movie_and_save_it(self):
-        movie = Movie(title = "Hej Ho", id = 1)
-        movie.save()
-        self.assertEqual(Movie.objects.count(), 1)
-
-    def test_add_movie_to_movie_popular_query(self):
-        movie1 = Movie.objects.create(title = "Hej Ho", id = 1)
-        mpq = MoviePopularQuery.objects.create(
-            timestamp = datetime.datetime.utcnow(), page = 1)
-        mpq.movies.add(movie1)
-        mpq.save()
-        movie2 = Movie.objects.first()
-        self.assertIn(movie2, MoviePopularQuery.objects.first().movies.all())
-
-
 class MovieTest(TestCase):
 
     @patch("tmdb.models.Movie.objects.get")
@@ -100,43 +83,101 @@ class NowPlayingQueryTest(TestCase):
         }
 
     def test_get_returns_npq_object(self, mock_tmdb):
+        mock_tmdb.return_value = self.tmdb_return
         npq = NowPlayingQuery.get(page = 1)
         self.assertIsInstance(npq, NowPlayingQuery)
 
     @patch ("tmdb.models.NowPlayingQuery.objects.get")
     def test_checks_for_presence_of_the_page_in_db(self, mock_get, mock_tmdb):
+        mock_tmdb.return_value = self.tmdb_return
         npq = NowPlayingQuery.get(page = 1)
         self.assertTrue(mock_get.called)
 
     def test_for_not_propagation_error_if_page_not_exist(self, mock_tmdb):
+        mock_tmdb.return_value = self.tmdb_return
         try:
             npq = NowPlayingQuery.get(page = 1)
         except NowPlayingQuery.DoesNotExist:
             self.fail("get raised DoesNotExist unexpectedly!")
 
     def test_returns_proper_npq_if_exists(self, mock_tmdb):
+        mock_tmdb.return_value = self.tmdb_return
         npq_base = NowPlayingQuery.objects.create(page = 1)
         npq = NowPlayingQuery.get(page = 1)
         self.assertEqual(npq, npq_base)
 
     def test_saves_new_npq_in_db(self, mock_tmdb):
+        mock_tmdb.return_value = self.tmdb_return
         npq = NowPlayingQuery.get(page = 1)
         self.assertEqual(NowPlayingQuery.objects.count(), 1)
 
     def test_calls_tmdb_request(self, mock_tmdb):
+        mock_tmdb.return_value = self.tmdb_return
         NowPlayingQuery.get(page = 1)
         mock_tmdb.assert_called_with(
             method = "GET", 
-            path = "movie/nowplaying", 
+            path = "movie/now_playing", 
             params = {"page": 1}
         )
 
     def test_saves_new_movies_in_db(self, mock_tmdb):
-        mock_tmdb.return_value.json.return_value = self.tmdb_return
+        mock_tmdb.return_value = self.tmdb_return
         NowPlayingQuery.get(page = 1)
         mock_tmdb.assert_called_with(
             method = "GET", 
-            path = "movie/nowplaying", 
+            path = "movie/now_playing", 
             params = {"page": 1}
         )
         self.assertTrue(Movie.objects.count(), 2)
+
+    def test_updates_only_selected_movies(self, mock_tmdb):
+        mock_tmdb.return_value = self.tmdb_return 
+        Movie.objects.create(id = 9, title = "Nine", update_level = 0)
+        Movie.objects.create(id = 41, title = "FortyOne", update_level = 100)
+        npq = NowPlayingQuery.get(page = 1)
+        self.assertEqual(Movie.objects.get(id = 41).title, "FortyOne")
+        self.assertNotEqual(Movie.objects.get(id = 9).title, "Nine")
+
+    def test_associates_movies_with_npq(self, mock_tmdb):
+        mock_tmdb.return_value = self.tmdb_return        
+        npq = NowPlayingQuery.get(page = 1)       
+        self.assertEqual(npq.movies.count(), 2)
+        self.assertEqual(
+            set(npq.movies.values_list("id", flat=True)), 
+            set([9, 41])
+        )
+
+    @patch("tmdb.models.Movie.save_movie_in_db")
+    def test_does_not_call_save_movie_in_db_if_not_required(
+        self, mock_save, mock_tmdb
+    ):
+        mock_tmdb.return_value = self.tmdb_return 
+        Movie.objects.create(id = 9, title = "Nine", update_level = 100)
+        Movie.objects.create(id = 41, title = "FortyOne", update_level = 100)   
+        npq = NowPlayingQuery.get(page = 1)
+        self.assertFalse(mock_save.called)
+
+    def test_updates_npq_and_movies_when_movies_with_low_update_level(self, mock_tmdb):
+        mock_tmdb.return_value = self.tmdb_return        
+        npq = NowPlayingQuery.objects.create(page = 1)
+        movie1 = Movie.objects.create(id = 9, title = "Nine", update_level = 0)
+        movie2 = Movie.objects.create(id = 41, title = "FortyOne", update_level = 100)   
+        npq.movies.add(movie1)
+
+        npq = NowPlayingQuery.get(page = 1)
+
+        self.assertTrue(Movie.objects.get(id = 9).title, "0940")
+        self.assertTrue(Movie.objects.get(id = 41).title, "FortyOne")
+        self.assertEqual(NowPlayingQuery.objects.first().movies.count(), 2)
+
+    def test_updates_npq_and_movies_when_force_update(self, mock_tmdb):
+        mock_tmdb.return_value = self.tmdb_return  
+        npq = NowPlayingQuery.objects.create(page = 1)
+        movie1 = Movie.objects.create(id = 9, title = "Nine", update_level = 100)
+        npq.movies.add(movie1)
+
+        npq = NowPlayingQuery.get(page = 1, force_update = True)
+
+        self.assertTrue(Movie.objects.get(id = 9).title, "Nine")
+        self.assertEqual(NowPlayingQuery.objects.first().movies.count(), 2)
+   
