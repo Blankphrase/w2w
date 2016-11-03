1e2d4170811ae1aba2d7eb3cb261c1a888404222
hakubaa <jakub.agatowski@gmail.com>
2016-11-03 22:55:21 +0100

    Footer and new images for posters issues

--- main/static/js/movieinfo.js
+++ main/static/js/movieinfo.js
@@ -13,35 +13,55 @@ $('#movieInfo').on('shown.bs.modal', function (event) {
     $.post(
         "/movies/" + movieId + "/info"
     ).done(function(data) {
-            modal.find("#minfo-loading").hide();           
-            modal.find("#minfo-body").show();
-            modal.find("#minfo-poster").attr(
-                "src", "https://image.tmdb.org/t/p/w154" + data.poster_path
-            );
-            modal.find("#minfo-title").html(
-                data.title + " (" + 
-                (new Date(data.release_date)).getFullYear() + ")"
-            );
-            modal.find("#minfo-overview").html(data.overview);
-            if (data.genres !== undefined) {
-                modal.find("#minfo-genres").html(
-                    data.genres.map(function(item) { return item.name }).join(", ")
-                );
+            if (data.w2w_status == "ERROR") {
+                $("#movieInfo").modal("hide");
+                alert("Error while loading information about the movie. Please try again latter.");
             } else {
-                modal.find("#minfo-genres").html(""); 
+                modal.find("#minfo-loading").hide();           
+                modal.find("#minfo-body").show();
+                modal.find("#minfo-poster").attr(
+                    "src", "https://image.tmdb.org/t/p/w154" + data.poster_path
+                );
+                modal.find("#minfo-title").html(
+                    (data.title === null ? "<Title not available>" : data.title) + 
+                    (data.release_date === null ? "" : 
+                        " (" + (new Date(data.release_date)).getFullYear() + ")")
+                );
+                modal.find("#minfo-overview").html(
+                    data.overview === null ? "<Overview not available>" : data.overview
+                );
+                if (data.genres !== undefined) {
+                    modal.find("#minfo-genres").html(
+                        data.genres.map(function(item) { return item.name }).join(", ")
+                    );
+                } else {
+                    modal.find("#minfo-genres").html(""); 
+                }
+
+                modal.find("#minfo-runtime").html(
+                    data.runtime === null ? "-" : data.runtime   
+                );
+
+                modal.find("#minfo-vote").html(
+                    (data.vote_average === null ? "-" : data.vote_average) +
+                    " (" + (data.vote_count === null ? "-" : data.vote_count) + 
+                    " votes)"
+                );
+                modal.find("#minfo-imdb").attr(
+                    "href",
+                    "http://www.imdb.com/title/" + data.imdb_id + "/"
+                );
+                modal.find("#minfo-tmdb").attr(
+                    "href",
+                    "https://www.themoviedb.org/movie/" + data.id
+                );
             }
-            modal.find("#minfo-runtime").html(data.runtime);
-            modal.find("#minfo-vote").html(
-                data.vote_average + " (" + data.vote_count + " votes)"
-            );
-            modal.find("#minfo-imdb").attr(
-                "href",
-                "http://www.imdb.com/title/" + data.imdb_id + "/"
-            );
-            modal.find("#minfo-tmdb").attr(
-                "href",
-                "https://www.themoviedb.org/movie/" + data.id
-            );
         }
     );
-})
+});
+
+$("#minfo-poster").on("load", function() { 
+    $(this).show();
+}).on("error", function() { 
+    $(this).hide();
+});
